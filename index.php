4fee5afe3d29fb58797f1847e8b4608e7afc0ecc
fnooub <61742373+fnooub@users.noreply.github.com>
2020-04-20 15:10:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,7 @@
     <head><meta charset="UTF-8"></head>
     <body>
         <h1>S3 upload example</h1>
-<?php
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
-	echo $_FILES['userfile']['tmp_name'];
-	echo "<br>";
-	echo $_FILES['userfile']['name'];
-}
-?>
+
         <h2>Upload a file</h2>
         <form enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
             <input name="userfile" type="file"><input type="submit" value="Upload">
